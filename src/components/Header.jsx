a087a59934fbcc8a5c7008009494197521321db8
nine5566 <pramuanthanatchaporn@gmail.com>
2026-03-03 21:20:28 +0700

    create header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Header component</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt magni ea ratione nihil. Suscipit numquam laudantium recusandae, autem ullam magni modi at soluta perspiciatis voluptas hic necessitatibus exercitationem quia vitae.</p>
+            
+            
+        </header>
+    )
+}
+
+export default Headers
